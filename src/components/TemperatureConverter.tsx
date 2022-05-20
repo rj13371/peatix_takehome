b0d216a23682292beaf02f5229e464601ec44510
rj1337 <mathesonroland@gmail.com>
2022-05-20 15:44:50 +0900

    add state for celcuis or farenheight conversion

--- src/components/TemperatureConverter.tsx
+++ src/components/TemperatureConverter.tsx
@@ -1,12 +1,16 @@
 import React, { ReactElement } from 'react';
-import { InputNumber, Button, Space, Typography } from 'antd';
+import { InputNumber, Button, Space, Typography, Switch } from 'antd';
 
-const CELSIUS_TO_FARENHEIGHT = (f: number): number => {
-  return (f * 9) / 5 + 32;
+interface ITemperature {
+  celsuisToFarenheight: boolean;
+}
+
+const CELSIUS_TO_FARENHEIGHT = (c: number): number => {
+  return (c * 9) / 5 + 32;
 };
 
 const FARENHEIGHT_TO_CELSIUS = (f: number): number => {
-  return f - 32 / 1.8;
+  return ((f - 32) * 5) / 9;
 };
 
 const { Title } = Typography;
@@ -14,23 +18,39 @@ const { Title } = Typography;
 export default function TemperatureConverter(): ReactElement {
   const [value, setValue] = React.useState<number>(0);
   const [result, setResult] = React.useState<number>(0);
+  const [conversion, setConversion] = React.useState<ITemperature>({
+    celsuisToFarenheight: true,
+  });
+
+  console.log(conversion, result, value);
 
   const convert = (n: number): void => {
-    setResult(CELSIUS_TO_FARENHEIGHT(n));
+    if (conversion.celsuisToFarenheight === true) {
+      setResult(CELSIUS_TO_FARENHEIGHT(n));
+    } else {
+      setResult(FARENHEIGHT_TO_CELSIUS(n));
+    }
+  };
+
+  const handleOnClick = (checked: boolean, event: MouseEvent) => {
+    event.preventDefault();
+    if (checked) {
+      setConversion({ celsuisToFarenheight: true });
+    } else {
+      setConversion({ celsuisToFarenheight: false });
+    }
   };
 
   return (
-    <Space>
+    <>
       <Title>{result}</Title>
+      <Switch
+        defaultChecked
+        onChange={handleOnClick}
+        checkedChildren="°C =>°F"
+        unCheckedChildren="°F =>°C"
+      />
       <InputNumber min={-100} max={100} value={value} onChange={convert} />
-      <Button
-        type="primary"
-        onClick={() => {
-          setValue(0);
-        }}
-      >
-        Reset
-      </Button>
-    </Space>
+    </>
   );
 }
